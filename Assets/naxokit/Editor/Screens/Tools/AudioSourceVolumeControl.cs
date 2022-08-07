04a8a2d0514703b7dbf981c2df79c0eb8613e802
HakuSystems <66133638+HakuSystems@users.noreply.github.com>
2022-08-07 02:31:49 +0200

    Added AudioSourceVolumeControl Tool

---
+++ Assets/naxokit/Editor/Screens/Tools/AudioSourceVolumeControl.cs
@@ -0,0 +1,99 @@
+using System;
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using naxokit.Styles;
+using naxokit.Helpers.Logger;
+
+public class AudioSourceVolumeControl : EditorWindow {
+
+    public static List<AudioSource> audioSources = new List<AudioSource>();
+    private Vector2 scrollPosition;
+    private int showindex = 10;
+    private string searchBar ="";
+
+    public static void ShowWindow() {
+        var window = GetWindow<AudioSourceVolumeControl>();
+        window.titleContent = new GUIContent("AudioSourceVolumeControl");
+        window.Show();
+    }
+
+    private void OnEnable(){
+        minSize = new Vector2(800, 300);
+        maxSize = new Vector2(800, 300);
+        audioSources.Clear();
+        foreach(var audioSource in GameObject.FindObjectsOfType<AudioSource>()) {
+            audioSources.Add(audioSource);
+        }
+    }
+    private void Update() {
+        Repaint();
+        if(audioSources.Count != GameObject.FindObjectsOfType<AudioSource>().Length) {
+            audioSources.Clear();
+            foreach(var audioSource in GameObject.FindObjectsOfType<AudioSource>()) {
+                audioSources.Add(audioSource);
+            }
+        }
+        
+    }
+    private void OnGUI() {
+        EditorGUILayout.BeginVertical();
+        {
+            EditorGUILayout.BeginHorizontal();
+            {
+                EditorGUILayout.LabelField($"Total: {audioSources.Count}", EditorStyles.toolbarButton);
+                EditorGUILayout.LabelField("AudioSources in Hierarchy", EditorStyles.toolbarButton);
+            }
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.BeginHorizontal();
+            {
+                showindex = EditorGUILayout.IntSlider("Show Amount: ",showindex, 0, audioSources.Count - 1);
+                searchBar = EditorGUILayout.TextField(searchBar, EditorStyles.toolbarSearchField);
+            }
+            EditorGUILayout.EndHorizontal();
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+            {
+                EditorGUILayout.BeginVertical();
+                {
+                    var resultCount = 0;
+                    foreach(var audioSource in audioSources) {
+                        resultCount++;
+                        if(audioSource.name.ToLower().Contains(searchBar.ToLower())&& showindex > resultCount - 1) {
+                            EditorGUILayout.BeginHorizontal();
+                            {
+                                if(audioSource.volume > 0.5f){
+                                    GUI.color = Color.green;
+                                } else if(audioSource.volume > 0.25f){
+                                    GUI.color = Color.yellow;
+                                } else {
+                                    GUI.color = Color.red;
+                                }                        
+                                if(audioSource != null){
+                                    GUILayout.Space(10);
+                                    EditorGUILayout.LabelField(audioSource.name, EditorStyles.largeLabel);
+                                    if(GUILayout.Button("Select in Hierachry", EditorStyles.toolbarButton))
+                                        Selection.activeGameObject = audioSource.gameObject;
+                                    if(GUI.color == Color.green){
+                                        EditorGUILayout.LabelField("GOOD TO HEAR", EditorStyles.toolbarButton);
+                                    } else if(GUI.color == Color.yellow){
+                                        EditorGUILayout.LabelField("LISTEN CLOSELY", EditorStyles.toolbarButton);
+                                    } else if(GUI.color == Color.red){
+                                        EditorGUILayout.LabelField("MUTE/ALMOST MUTE", EditorStyles.toolbarButton);
+                                    }
+                                    audioSource.volume = EditorGUILayout.Slider(audioSource.volume, 0, 1);
+                                
+                                }
+                                
+                            }
+                            EditorGUILayout.EndHorizontal();
+                        }
+                        else{resultCount--;}
+                    }
+                }
+                EditorGUILayout.EndVertical();
+            }
+            EditorGUILayout.EndScrollView();
+        }
+        EditorGUILayout.EndVertical();
+    }
+}
